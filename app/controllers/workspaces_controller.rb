3d60d90e4e599321eaa61b0507f325f25ef7a57b
prajitlal4 <prajit.lal4@gmail.com>
2024-01-07 23:04:57 +0800

    form to add new workspaces.

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -3,8 +3,32 @@ class WorkspacesController < ApplicationController
     @workspaces = Workspace.joins(:workspace_members).where(workspace_members: { user_id: current_user.id })
   end
 
+  def new
+    @workspace = Workspace.new
+  end
+
+  def create
+    @workspace = Workspace.new(workspace_params)
+    @workspace.created_by_user = current_user
+
+    if @workspace.save
+      # Automatically create a membership for the user who created the workspace
+      WorkspaceMember.create!(workspace: @workspace, user: current_user)
+
+      redirect_to @workspace, notice: 'Workspace was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def show
     @workspace = Workspace.find(params[:id])
+  end
+
+  private
+
+  def workspace_params
+    params.require(:workspace).permit(:name, :description, :user_id)
   end
 
 end
