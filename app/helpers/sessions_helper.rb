31b6e4b96bf6f9fef2b79eb85b49fe037f826c4f
prajitlal4 <prajit.lal4@gmail.com>
2024-01-13 19:53:33 +0800

    added methods to show either recent reps / sets / weights or 0 if its a new session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+  def last_value_for_exercise(exercise, attribute)
+    SessionExercise.joins(:routine_exercise)
+                   .where(routine_exercises: { exercise_id: exercise.id })
+                   .order(created_at: :desc)
+                   .pick(attribute) || 0
+  end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,9 @@ class Session < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :session_exercises, dependent: :destroy
+
+  def ended?
+    end_time.present?
+  end
+
 end
