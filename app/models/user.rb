51291dada4603b3fa5847e6749b89166b44e444a
prajitlal4 <prajit.lal4@gmail.com>
2024-01-25 18:08:44 +0800

    added back method to add user if user already exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,21 @@ class User < ApplicationRecord
 
   def self.invite!(attributes = {}, invited_by = nil)
     invited_user = super(attributes.except(:group_id), invited_by)
-    if invited_user.persisted? && attributes[:group_id]
-      InvitationGroupAssociation.create!(
-        email: invited_user.email,
-        group_id: attributes[:group_id],
-        invitation_token: invited_user.raw_invitation_token
-      )
+    user = User.find_by(email: invited_user.email)
+    if user
+      group = Group.find(attributes[:group_id])
+      unless GroupMember.exists?(user: user, group: group)
+        GroupMember.create(user: user, group: group)
+      end
+    else
+      if invited_user.persisted? && attributes[:group_id]
+        InvitationGroupAssociation.create!(
+          email: invited_user.email,
+          group_id: attributes[:group_id],
+          invitation_token: invited_user.raw_invitation_token
+        )
+      end
     end
-
     invited_user
   end
 end
