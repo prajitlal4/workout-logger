08281503e84126c5be3f676cacefe76bfc20e702
prajitlal4 <prajit.lal4@gmail.com>
2024-01-25 18:19:15 +0800

    fixed alerts not showing properly

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
   before_action :check_membership, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @owned_groups = Group.where(created_by_user: current_user)
