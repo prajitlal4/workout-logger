096c3cf541bb169471e9a8fdeb52427e9943b8ab
prajitlal4 <prajit.lal4@gmail.com>
2024-01-29 20:16:37 +0800

    populating seeds with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = Categories.create([{name: "Chest", description:"Chest exercises", user_id: nil }, {name: "Back", description:"Back exercises", user_id: nil }, {name: "Legs", description:"Leg exercises", user_id: nil }, {name: "Shoulders", description:"Shoulder exercises", user_id: nil }, {name: "Biceps", description:"Bicep exercises", user_id: nil }, {name: "Triceps", description:"Tricep exercises", user_id: nil }, {name: "Abs", description:"Core exercises", user_id: nil }])
